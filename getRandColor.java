2b7a2d2e56c5531f6a7345d4cf5f8c00924de1ca
valix93 <v.lipari@studenti.unina.it>
2019-08-09 16:43:36 +0200

    Update getRandColor.java

--- getRandColor.java
+++ getRandColor.java
@@ -1,8 +1,14 @@
-protected int getRandColor() {
-		Random color = new Random();
+import android.graphics.Color;
 
-		int randomColor = Color.rgb(color.nextInt(256), color.nextInt(256),
-				color.nextInt(256));
+import java.util.Random;
 
-		return randomColor;
-	}
+public class Utils {
+    public static int getRandomColor() {
+        Random color = new Random();
+
+        int randomColor = Color.rgb(color.nextInt(256), color.nextInt(256),
+                color.nextInt(256));
+
+        return randomColor;
+    }
+}
