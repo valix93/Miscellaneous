89088477423a3d054d2386d292a132d169af016f
Valix93 <v.lipari@studenti.unina.it>
2021-05-03 12:29:51 +0200

    Create vis_count.js

---
+++ vis_count.js
@@ -0,0 +1,7 @@
+var divVisitors = document.getElementById("onlinevisitors");
+    divVisitors.style.textAlign = "center";
+    document.getElementById("live").innerHTML = "Online: " + last_vis_count;
+
+var divCenter = document.getElementById("center");
+    divCenter.style.textAlign = "center";
+    document.getElementById("contatore").innerHTML = "Visitatori totali: " + vis_count;
